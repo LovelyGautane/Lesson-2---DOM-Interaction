08573c80def652b0bfb7e870743e4bd38e22c467
LovelyGautane <gautanelovely96@gmail.com>
2021-11-19 21:32:48 +0800

    Activity 1 - Add image and value

--- Activity 1 - Data Binding Problem/app.js
+++ Activity 1 - Data Binding Problem/app.js
@@ -2,12 +2,21 @@ const app = Vue.createApp({
     data() {
         return {
             myName: 'Lovely Gautane',
-            myAge: 21
+            myAge: 21,
+            url: 'https://segwitz.com/wp-content/uploads/2021/06/vuejs-development-malaysia.jpeg'
         };
     },
     methods: {
         age() {
             return this.myAge + 5;
+        },
+        Uno() {
+            const random = Math.random();
+            if(random < 0.5) {
+                return '0'
+            } else {
+                return '1'
+            }
         }
     }
 });
