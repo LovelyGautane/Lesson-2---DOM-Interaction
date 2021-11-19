b92d6461eaadeaa00b7aaf6734d62fa26b44345d
LovelyGautane <gautanelovely96@gmail.com>
2021-11-19 21:26:02 +0800

    Activity 1 - Age in 5 years

--- Activity 1 - Data Binding Problem/app.js
+++ Activity 1 - Data Binding Problem/app.js
@@ -2,6 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             myName: 'Lovely Gautane',
+            myAge: 21
+        };
+    },
+    methods: {
+        age() {
+            return this.myAge + 5;
         }
     }
 });
